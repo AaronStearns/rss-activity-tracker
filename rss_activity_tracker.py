d09aade0607244de3737be415d693f37ff570674
AaronStearns <stearnsconcepts@gmail.com>
2021-01-26 19:31:58 -0500

    Adding rss_dict of companies and their rss feed urls

--- rss_activity_tracker.py
+++ rss_activity_tracker.py
@@ -1,17 +1,31 @@
 import requests
 import xmltodict
 
+"""
+rss_dict = { 
+  "crime_junkie": { "rss_urls": ['https://feeds.megaphone.fm/ADL9840290619', 'https://feeds.megaphone.fm/ADL9840290619'] }, 
+  "new_york_times": { "rss_urls": ['https://feeds.simplecast.com/54nAGcIl'] } }
+
+for key in rss_dict:
+  for rss_url in rss_dict[key]['rss_urls']:
+    print(rss_url)
+
+"""
 
 url = "https://feeds.megaphone.fm/ADL9840290619"
 response = requests.get(url)
 data = xmltodict.parse(response.content)
 
 most_recent_published = []
-for i, key in enumerate(data['rss']['channel']['item']): 
-  if i == 0:
-    most_recent_published.append(key['pubDate'])
+try:
+  for i, key in enumerate(data['rss']['channel']['item']): 
+    if i == 0:
+      most_recent_published.append(key['pubDate'])
 
-print(most_recent_published)
+  print(most_recent_published[0])
+except:
+  # move to next company key in rss_dict
+  print("nah")
 
 
 
