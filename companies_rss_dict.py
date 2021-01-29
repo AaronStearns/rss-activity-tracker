853891f2beaabce92d0c85168a91bc0c2773a288
AaronStearns <stearnsconcepts@gmail.com>
2021-01-28 21:24:40 -0500

    Adding type checking for args passed to main function. Creating helpful error messages

---
+++ companies_rss_dict.py
@@ -0,0 +1,7 @@
+# URLs stored as lists in case a company has more than one rss url
+rss_dict = { 
+  "crime_junkie": { "rss_urls": ['https://feeds.megaphone.fm/ADL9840290619'] }, 
+  "dateline": { "rss_urls": ['https://podcastfeeds.nbcnews.com/HL4TzgYC'] },
+  "new_york_times": { "rss_urls": ['https://feeds.simplecast.com/54nAGcIl'] } ,
+  "my_favorite_murder": { "rss_urls": ['https://www.omnycontent.com/d/playlist/aaea4e69-af51-495e-afc9-a9760146922b/44bbf446-4627-4f83-a7fd-ab070007db11/72b96aa8-88bd-480a-87af-ab070007db36/podcast.rss'] }
+}
